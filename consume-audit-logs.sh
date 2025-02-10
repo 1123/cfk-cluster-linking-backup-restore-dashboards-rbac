ccf1089b46581e077f461bcdff2de5c1d60657bb
Benedikt Linse <benedikt@confluent.io>
2025-02-10 13:04:21 +0100

    script for consuming audit logs via the kafka-console-consumer cli

---
+++ consume-audit-logs.sh
@@ -0,0 +1,11 @@
+kubectl exec kafka-0 -n confluent -- \
+  kafka-console-consumer \
+    --topic confluent-audit-log-events \
+    --bootstrap-server kafka.confluent.svc.cluster.local:9071 \
+    --consumer-property commit.interval.ms=10 \
+    --consumer-property sasl.mechanism=PLAIN \
+    --consumer-property security.protocol=SASL_SSL \
+    --consumer-property ssl.truststore.location=/mnt/sslcerts/truststore.jks \
+    --consumer-property ssl.truststore.password=mystorepassword \
+    --consumer-property "sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username=kafka password=kafka-secret;" \
+    --from-beginning
