3cdb4f8c10fd6fa4967d6e615acbf3dd62a6082b
Benedikt Linse <benedikt@confluent.io>
2025-02-19 17:32:01 +0100

    refactoring

--- backup-restore/start-backup-cluster.sh
+++ backup-restore/start-backup-cluster.sh
@@ -1,2 +1,8 @@
-kubectl annotate kraftcontroller backup-kraft platform.confluent.io/block-reconcile=false -n confluent --overwrite
-kubectl annotate kafka backup-kafka platform.confluent.io/block-reconcile=false -n confluent --overwrite
+kubectl annotate kraftcontroller backup-kraft \
+  platform.confluent.io/block-reconcile=false \
+  -n confluent \
+  --overwrite
+kubectl annotate kafka backup-kafka \
+  platform.confluent.io/block-reconcile=false \
+  -n confluent \
+  --overwrite
---
+++ data-migration/pause-cluster-link.sh
@@ -0,0 +1,1 @@
+kubectl apply -f strimzi-to-cp-link-paused.yaml
