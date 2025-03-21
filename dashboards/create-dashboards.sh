95c1bb3ae737c0c79a56895ae213c5aa24a776ed
Benedikt Linse <benedik@confluent.io>
2025-03-21 10:59:51 +0000

    removing dysfunctional dashboards

--- dashboards/create-dashboards.sh
+++ dashboards/create-dashboards.sh
@@ -1,4 +1,4 @@
-for dashboard in cluster-linking.json kafka-connect-cluster.json kafka-quotas.json schema-registry-cluster.json confluent-platform.json kafka-consumer.json kafka-topics.json kafka-cluster.json kafka-producer.json; do
+for dashboard in cluster-linking.json kafka-connect-cluster.json kafka-topics.json kafka-cluster.json ; do
   basename=$(basename $dashboard .json) 
   echo kubectl create configmap -n confluent $basename --from-file $dashboard
   echo kubectl label configmap $basename grafana_dashboard=1
